6fc324db398413a358c85bc6261d230ee161bfec
snake <lib0707@gmail.com>
2017-11-08 08:56:07 +0800

    新增存储数据的方法

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameControl : Singleton<GameControl> {
+    
+    protected GameControl(){} //Guarantee this will be always a singleton only - can't use the constructor!
+
+    public float health = 100f;
+    public float exp = 50f;
+
+    #region SaveData
+    public void Save(){
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+        PlayerData data = new PlayerData();
+        data.health = health;
+        data.exp = exp;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+    #endregion
+
+    #region LoadData
+    public void Load(){
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat",FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+            health = data.health;
+            exp = data.exp;
+        }
+    }
+    #endregion
+}
+
+[Serializable]
+class PlayerData{
+    public float health;
+    public float exp;
+}
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -10,6 +10,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         get{ 
             if (applicationIsQuitting)
             {
+                //If application is quitting, don't create again - return null
                 return null;
             }
             lock (_lock)
@@ -19,7 +20,10 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
                     _instance = (T)FindObjectOfType(typeof(T));
 
                     if (FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        //There should not be more than 1 singleton!
                         return _instance;
+                    }
 
                     if (_instance == null)
                     {
@@ -27,6 +31,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
                         _instance = singleton.AddComponent<T>();
                         singleton.name = "(singleton)" + typeof(T).ToString();
 
+                        //Do not destroy on load!
                         DontDestroyOnLoad(singleton);
                     }
                 }
@@ -37,6 +42,14 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// When Unity quits, it destroys objects in a random order.
+    /// In principle, a Singleton is only destroyed when application quits.
+    /// If any script calls Instance after it have been destroyed, 
+    /// it will create a buggy ghost object that will stay on the Editor scene
+    /// even after stopping playing the Application. Really bad!
+    /// So, this was made to be sure we're not creating that buggy ghost object.
+    /// </summary>
     public void OnDestroy(){
         applicationIsQuitting = true;
     }
